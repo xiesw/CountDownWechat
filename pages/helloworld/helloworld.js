2986cd29ee7620ee5e57e23ce2c501e51d1cde07
pain.xie <pain.xie@wolaidai.com>
2019-01-07 22:34:32 +0800

    wxss

--- pages/helloworld/helloworld.js
+++ pages/helloworld/helloworld.js
@@ -1,7 +1,18 @@
 Page({
   data: {
-    message: "谢尚雾",
-    color: "blue",
-    flag: true,
+    items: [
+      {name: 1},
+      {name: 2},
+      {name: 3},
+      {name: 4},
+      {name: 5}
+    ],
+    condition: Math.ceil(Math.random()*3),
+    tem: {
+      name: "谢尚雾",
+      phone: "13881116467",
+      address: "中国"
+    },
+    colorValue: '#342'
   }
 })
