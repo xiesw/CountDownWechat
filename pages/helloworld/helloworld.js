9d894da78f4ef5aa0782a4edf05afb6b7ea64ebf
pain.xie <pain.xie@wolaidai.com>
2019-01-08 18:12:44 +0800

    test

--- pages/helloworld/helloworld.js
+++ pages/helloworld/helloworld.js
@@ -1,18 +1,89 @@
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
     items: [
-      {name: 1},
-      {name: 2},
-      {name: 3},
-      {name: 4},
-      {name: 5}
+      { name: 1 },
+      { name: 2 },
+      { name: 3 },
+      { name: 4 },
+      { name: 5 }
     ],
-    condition: Math.ceil(Math.random()*3),
+    condition: Math.ceil(Math.random() * 3),
     tem: {
       name: "谢尚雾",
       phone: "13881116467",
       address: "中国"
     },
     colorValue: '#342'
+  },
+
+  clickMe(e) {
+    console.log(e);
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log("onLoad");
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    console.log("onReady");
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    console.log("onShow");
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    console.log("onLoad");
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    console.log("onUnload");
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    console.log("onPullDownRefresh");
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    console.log("onReachBottom");
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    console.log("onShareAppMessage");
+
   }
 })
